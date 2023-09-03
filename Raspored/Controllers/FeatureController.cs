a479caddf76330e4f1ae2c450256221742ac44d3
Bojan2111 <bojan.adzic@gmail.com>
2023-09-03 23:39:30 +0200

    Edit FeatureController

--- Raspored/Controllers/FeatureController.cs
+++ Raspored/Controllers/FeatureController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Raspored.Interfaces;
 using Raspored.Models.Login;
+using System.Threading.Tasks;
 
 namespace Raspored.Controllers
 {
@@ -12,28 +13,60 @@ namespace Raspored.Controllers
     public class FeatureController : ControllerBase
     {
         private readonly IFeatureRepository _featureRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public FeatureController(IFeatureRepository featureRepository)
+        public FeatureController(IFeatureRepository featureRepository, UserManager<ApplicationUser> userManager)
         {
             _featureRepository = featureRepository;
+            _userManager = userManager;
         }
 
-        [HttpGet]
-        [Route("/api/admin-features")]
-        //[Authorize(Roles = "admin")]
-        public IActionResult ListFeaturesForAdmin()
+        [HttpGet("/api/role-features")]
+        [Authorize]
+        public async Task<IActionResult> ListFeatures()
         {
-            var features = _featureRepository.GetFeaturesForRole("admin");
-            return Ok(features);
-        }
+            // Get the current user
+            _ = await _userManager.GetUserAsync(User);
 
-        [HttpGet]
-        [Route("/api/regular-features")]
-        //[Authorize(Roles = "zaposleni")]
-        public IActionResult ListFeaturesForEmployee()
-        {
-            var features = _featureRepository.GetFeaturesForRole("zaposleni");
-            return Ok(features);
+            // Check if the user has a role
+            if (User.IsInRole("admin"))
+            {
+                var features = _featureRepository.GetFeaturesForRole("admin");
+                return Ok(features);
+            }
+            else if (User.IsInRole("employee"))
+            {
+                var features = _featureRepository.GetFeaturesForRole("employee");
+                return Ok(features);
+            }
+            else
+            {
+                return Unauthorized();
+            }
         }
+        //private readonly IFeatureRepository _featureRepository;
+
+        //public FeatureController(IFeatureRepository featureRepository)
+        //{
+        //    _featureRepository = featureRepository;
+        //}
+
+        //[HttpGet]
+        //[Route("/api/admin-features")]
+        ////[Authorize(Roles = "admin")]
+        //public IActionResult ListFeaturesForAdmin()
+        //{
+        //    var features = _featureRepository.GetFeaturesForRole("admin");
+        //    return Ok(features);
+        //}
+
+        //[HttpGet]
+        //[Route("/api/regular-features")]
+        ////[Authorize(Roles = "zaposleni")]
+        //public IActionResult ListFeaturesForEmployee()
+        //{
+        //    var features = _featureRepository.GetFeaturesForRole("zaposleni");
+        //    return Ok(features);
+        //}
     }
 }
