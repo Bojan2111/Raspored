658478c3c4b123662853222d6fb51aa4d7fe8210
Bojan2111 <bojan.adzic@gmail.com>
2023-10-28 23:17:46 +0200

    Edit TeamMemberDTO

--- Raspored/Models/DTOs/TeamMemberDTO.cs
+++ Raspored/Models/DTOs/TeamMemberDTO.cs
@@ -5,7 +5,9 @@
         public int Id { get; set; }
         public string UserId { get; set; }
         public string Name { get; set; }
+        public int YearOfEmployment { get; set; }
         public string TeamName { get; set; }
+        public string Position { get; set; }
         public string Role { get; set; }
     }
 }
--- Raspored/Models/DTOs/TeamMemberProfile.cs
+++ Raspored/Models/DTOs/TeamMemberProfile.cs
@@ -10,6 +10,8 @@ namespace Raspored.Models.DTOs
                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.Id))
                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.User.LastName} {src.User.FirstName}"))
+               .ForMember(dest => dest.YearOfEmployment, opt => opt.MapFrom(src => src.User.YearOfEmployment))
+               .ForMember(dest => dest.Position, opt => opt.MapFrom(src => src.User.Position.Name))
                .ForMember(dest => dest.TeamName, opt => opt.MapFrom(src => src.Team.Name))
                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.TeamMemberRole.Name));
         }
